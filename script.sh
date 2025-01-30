77090abf5a5432cdd4f9eb7832583859f629475d
janekeyes <janekeyes123@gmail.com>
2025-01-30 12:48:13 +0000

    Initial commit with script and .gitignore

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#create folders
+mkdir fol_1 fol_2
+
+#create files in fol_1
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+
+#create files in fol_2
+touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+
+# Print completion message
+echo "Job completed."
