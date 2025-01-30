506677a3fc3c8b788cfe02853d19bcb1082b887f
janekeyes <janekeyes123@gmail.com>
2025-01-30 12:53:44 +0000

    Updated file permissions in script

--- script.sh
+++ script.sh
@@ -12,6 +12,10 @@ touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 #create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 
+# Set permissions
+chmod 600 fol_1/1_1.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt fol_2/2_2.txt
+
 
 # Print completion message
 echo "Job completed."
